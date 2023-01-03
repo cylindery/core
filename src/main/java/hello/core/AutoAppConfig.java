02979cfef15e1921a10037d54614682114351c42
juwonju <jxwxnjx@gmail.com>
2023-01-03 19:28:10 +0900

    chore: 의존관계 주입

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,7 +1,10 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,14 +12,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-//        basePackages = "hello.core",
-//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
-//    @Bean("memoryMemberRepository")
-//    MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
+    @Bean
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
